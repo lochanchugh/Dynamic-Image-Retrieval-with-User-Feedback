55bc531edeab6464185fe326b38c5b29cb7c944c
Lochan <98405764+lochanchugh@users.noreply.github.com>
2025-03-21 19:29:14 +0530

    Initial Commit

---
+++ Image Retrieval/Image_RetrievalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_RetrievalApp.swift
+//  Image Retrieval
+//
+//  Created by Lochan on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Image_RetrievalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
